6d7cbe56adc998da70059d043a6c1e834f52fdc1
Jonny Burger <jonathanburger11@gmail.com>
2023-06-22 14:52:28 +0200

    Release 4.1.0-alpha3

--- goclient.go
+++
@@ -1,9 +0,0 @@
-package lambda_go_sdk
-
-func RenderMediaOnLambda(input RemotionOptions) (*RemotionRenderResponse, error) {
-	return invokeRenderLambda(input)
-}
-
-func GetRenderProgress(input RenderConfig) (*RenderProgressResponse, error) {
-	return invokeRenderProgressLambda(input)
-}
